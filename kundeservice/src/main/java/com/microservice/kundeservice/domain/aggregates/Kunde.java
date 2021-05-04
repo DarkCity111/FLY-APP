1a11bee066492e21010eed645670fd219dbf7bfc
DarkCity111 <If62634321>
2021-05-04 21:44:57 +0200

    aggregate angelegt

---
+++ kundeservice/src/main/java/com/microservice/kundeservice/domain/aggregates/Kunde.java
@@ -0,0 +1,42 @@
+package com.microservice.kundeservice.domain.aggregates;
+
+import com.microservice.kundeservice.domain.valueobjects.PersonName;
+import com.microservice.kundeservice.domain.valueobjects.PersonOrt;
+import com.microservice.kundeservice.domain.valueobjects.PersonPLZ;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Kunde {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Embedded
+    @NotNull
+    private PersonName vorname;
+
+    @Embedded
+    @NotNull
+    private PersonName nachname;
+
+    @Embedded
+    @NotNull
+    private PersonPLZ plz;
+
+    @Embedded
+    @NotNull
+    private PersonOrt ort;
+
+
+
+
+}
