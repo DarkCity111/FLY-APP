6114e6a42b74238a1fe29a2de47ff030789b93d5
DarkCity111 <If62634321>
2021-05-04 23:12:22 +0200

    CommandService createKunde methode implementiert

--- kundeservice/src/main/java/com/microservice/kundeservice/services/CommandService.java
+++ kundeservice/src/main/java/com/microservice/kundeservice/services/CommandService.java
@@ -1,5 +1,7 @@
 package com.microservice.kundeservice.services;
 
+import com.microservice.kundeservice.domain.aggregates.Kunde;
+import com.microservice.kundeservice.domain.commands.CreateKundeCommand;
 import com.microservice.kundeservice.infrastructure.repositories.KundenRepository;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,19 @@ public class CommandService {
         this.queryService = queryService;
     }
 
+    public String createKunden(CreateKundeCommand createKundeCommand){
+        System.out.println("Create Kunden Service CreateStudentCommand");
 
+        Kunde kunde = new Kunde(createKundeCommand);
+
+        //Abfrage ob Kundennummer schon vorhanden
+        if(queryService.kundennummerInUse(kunde.getKundennummer().getKundennummer())){
+            throw new IllegalArgumentException("Kundennummer bereits vorhanden FEHLER CommandService Zeile 25");
+        }else{
+            kundenRepository.save(kunde);
+            return kunde.getKundennummer().getKundennummer();
+        }
+    }
 
 
 
