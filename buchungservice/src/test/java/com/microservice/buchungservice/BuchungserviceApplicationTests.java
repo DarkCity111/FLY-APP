bdffc2bbc18139a5ad9529c15ec1254f4fa0b503
DarkCity111 <If62634321>
2021-05-05 00:20:28 +0200

    init buchungservice

---
+++ buchungservice/src/test/java/com/microservice/buchungservice/BuchungserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.buchungservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuchungserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
