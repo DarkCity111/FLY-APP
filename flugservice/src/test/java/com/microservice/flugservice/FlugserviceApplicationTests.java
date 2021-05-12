48735cac76999b063237a809a73baa59411d1cc2
DarkCity111 <If62634321>
2021-05-12 05:57:31 +0200

    application.properties aktualisiert

---
+++ flugservice/src/test/java/com/microservice/flugservice/FlugserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.flugservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlugserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
